5c525d8f7cd2325e5742ab9ffd5ac793c80c3b44
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-10 01:11:46 +0800

    更新 jiuchuipeiyin.js

--- jiuchuipeiyin.js
+++ jiuchuipeiyin.js
@@ -3,7 +3,7 @@
 九锤配音
 
 [rewrite_local]
-https://api.next.bspapp.com/client script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/jiuchuipeiyin.js
+https://api.next.bspapp.com/client url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/jiuchuipeiyin.js
 [mitm] 
 hostname = api.next.bspapp.com
 
