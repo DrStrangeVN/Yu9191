794f677d17edd541303e0db0fdf4449ccb58170f
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-21 15:08:09 +0800

    更新 jikeshijian.js

--- jikeshijian.js
+++ jikeshijian.js
@@ -6,7 +6,7 @@
 [rewrite_local]
 
 
-https://time.geekbang.org/serv/v1/column/articles url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/jikeshijian.js
+https://time.geekbang.org/serv/v1 url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/jikeshijian.js
 
 [mitm] 
 
@@ -17,10 +17,11 @@ hostname = time.geekbang.org
 
 var body = $response.body;
 
-
 body = body.replace(/"article_could_preview":\w+/g, '"article_could_preview":true');
 
 body = body.replace(/article_could_preview":".*?"/g,'article_could_preview":true');
 
+body = body.replace(/is_video_preview":".*?"/g,'is_video_preview":true');
 
+body = body.replace(/column_had_sub":".*?"/g,'column_had_sub":true');
 $done({ body });
