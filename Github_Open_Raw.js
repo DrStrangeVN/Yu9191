ffca12521c82d3faa5da6a960956e6b4ce314948
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-23 03:56:35 +0800

    Update Github_Open_Raw.js

--- Github_Open_Raw.js
+++ Github_Open_Raw.js
@@ -1,34 +1,49 @@
 // ==UserScript==
-// @name         GitHub"打开URL链接"
+// @name         GitHub Raw Link Opener
 // @namespace    ios151
-// @version      1.5
-// @description  适用于addons,stay扩展Js,更好的体验Github
+// @version      1.6
+// @description  Enhance the GitHub raw link button.
 // @author       baby
 // @match        https://github.com/*
-// @grant        可搭配Github汉化：https://github.com/maboloshi/github-chinese/raw/gh-pages/main.user.js
-
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    function openRawLink() {
-        var rawUrl = window.location.href.replace('/blob', '').replace('github.com', 'raw.githubusercontent.com');
-        window.location.href = rawUrl;
+    // Check if the current page is a GitHub blob page
+    const isBlobPage = window.location.pathname.includes('/blob/');
+    
+    // Wait for the DOM to be fully loaded
+    if (document.readyState === 'complete' || (document.readyState !== 'loading' && !isBlobPage)) {
+        init();
+    } else {
+        document.addEventListener('DOMContentLoaded', init);
     }
 
-    var rawButton = document.createElement('button');
-    rawButton.innerHTML = '<i class="fas fa-external-link-alt"></i> 打开 Raw'; // 添加图标和文本
-    rawButton.style.position = 'fixed';
-    rawButton.style.right = '20px'; // 右侧距离
-    rawButton.style.bottom = '20px'; // 底部距离
-    rawButton.style.backgroundColor = '#333'; // 背景颜色
-    rawButton.style.color = '#fff'; // 文本颜色
-    rawButton.style.border = 'none';
-    rawButton.style.padding = '8px 16px';
-    rawButton.style.borderRadius = '5px';
-    rawButton.style.cursor = 'pointer';
-    rawButton.addEventListener('click', openRawLink);
+    function init() {
+        if (isBlobPage) {
+            
+            const rawButton = document.createElement('button');
+            rawButton.innerHTML = '<i class="fas fa-external-link-alt"></i> 打开 Raw';
+            rawButton.style.position = 'fixed';
+            rawButton.style.right = '20px';
+            rawButton.style.bottom = '20px';
+            rawButton.style.backgroundColor = '#333';
+            rawButton.style.color = '#fff';
+            rawButton.style.border = 'none';
+            rawButton.style.padding = '8px 16px';
+            rawButton.style.borderRadius = '5px';
+            rawButton.style.cursor = 'pointer';
+            rawButton.addEventListener('click', openRawLink);
+            document.body.appendChild(rawButton);
+        }
+    }
 
-    document.body.appendChild(rawButton);
+    function openRawLink() {
+     
+        const rawUrl = window.location.href.replace('/blob', '').replace('github.com', 'raw.githubusercontent.com');
+
+        window.open(rawUrl, '_blank');
+    }
 })();
