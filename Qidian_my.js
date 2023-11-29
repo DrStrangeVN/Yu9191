7c1911c9abf5a575c8e890c581ccdb1bd70e290b
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-29 16:10:46 +0800

    Update Qidian_my.js

--- Qidian_my.js
+++ Qidian_my.js
@@ -3,6 +3,7 @@
 # 我的界面
 ^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v3\/user\/getaccountpage url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Qidian_my.js
 # 我的界面内测推广
+^http[s]?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/bookshelf\/refresh url reject-200
 ^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch url reject
 #书架置顶推广(保留签到移除花哨背景)
 ^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/bookshelf\/getTopOperation url reject-dict
