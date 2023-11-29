a2d430bb1b1e754fe9e53d8ef263848bce839919
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-29 18:11:05 +0800

    Update Qidian_my.js

--- Qidian_my.js
+++ Qidian_my.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
-# 我的界面
-^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v3\/user\/getaccountpage url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Qidian_my.js
+# 我的界面及活动按钮？
+^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/(v1\/assembly\/toolbar|v3\/user\/getaccountpage) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Qidian_my.js
 # 我的界面内测推广
 ^http[s]?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/bookshelf\/refresh url reject-200
 ^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/adv\/getadvlistbatch url reject
@@ -12,8 +12,20 @@
 [mitm] 
 hostname = magev6.if.qidian.com
 */
-var Q = JSON.parse($response.body);
-Q.Data.BenefitButtonList = [];//福利中心
-Q.Data.FunctionButtonList = [];//我发布的
-Q.Data.BottomButtonList = [];//帮助与客服
-$done({body : JSON.stringify(Q)});
+^https:\/\/magev6\.if\.qidian\.com\/argus\/api\/(v1\/assembly\/toolbar|v3\/user\/getaccountpage)
+
+
+
+if ($request.url.includes("argus/api/v3/user/getaccountpage")) {
+  var Q = JSON.parse($response.body);
+  Q.Data.BenefitButtonList = [];//福利中心
+  Q.Data.FunctionButtonList = [];//我发布的
+  Q.Data.BottomButtonList = [];//帮助与客服
+  Q.Data.AccountBalance.Hints = [];
+  Q.Data.Member = null;
+  $done({body : JSON.stringify(Q)});
+} else if ($request.url.includes("argus/api/v1/assembly/toolbar")) {
+  var Q = JSON.parse($response.body);
+  Q.Data.Toolbar.Adv = {};
+  $done({body : JSON.stringify(Q)});
+}
