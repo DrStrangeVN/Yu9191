807f49e37f7585c6b1401be83ee48f73d4e7eba6
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-12-21 15:45:20 +0800

    Update everyday.js

--- everyday.js
+++ everyday.js
@@ -11,7 +11,7 @@ hostname = api.everyday.app
 
 */
 let obj = JSON.parse($response.body);
-obj.plan = "yaer";//终身lifetime
+obj.plan = "year"; 
 obj.premium_access = "2099-09-09T00:00:00.000Z";
-obj.last_action = 2023-12-20T19:02:52.000Z";
+obj.last_action = "2023-12-20T19:02:52.000Z"; 
 $done({ body: JSON.stringify(obj) });
