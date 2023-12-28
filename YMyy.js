d36993e13140340b30bf7b4c95f58d7469df8be7
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-12-28 20:14:47 +0800

    Create YMyy.js

---
+++ YMyy.js
@@ -0,0 +1,42 @@
+/*
+游美英语
+2023.12.28
+[rewrite_local]
+#用户信息
+^https:\/\/www\.usacamp\.cn\/Api1_2\/loginWithUserID url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Ymyy.js
+#个人中心推广
+https://www.usacamp.cn/Api1_2/getGuideBanner url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Ymyy.js
+
+[mitm]
+hostname = www.usacamp.cn
+*/
+
+let obj = JSON.parse($response.body);
+const url = $request.url;
+
+const yhxx = '/Api1_2/loginWithUserID';
+const grzx = '/Api1_2/getGuideBanner';
+
+if (url.indexOf(yhxx) != -1) {
+    let body = $response.body;
+    body = body.replace(/"end_time":"[^"]*"/g, '"end_time":"2099-09-29"');
+    //开始时间
+    body = body.replace(/"begin_time":"[^"]*"/g, '"begin_time":"2023-12-28"');
+    //是否体验
+    body = body.replace(/"bExperience":"[^"]*"/g, '"bExperience":"0"');
+    //体验天数
+    body = body.replace(/"experiencedays":"[^"]*"/g, '"experiencedays":"999999"');
+    //选择等级12
+    body = body.replace(/"selected_level":"[^"]*"/g, '"selected_level":"12"');
+    //可用天数
+    body = body.replace(/"availdays":"[^"]*"/g, '"availdays":"999999"');
+    //体验日期
+    body = body.replace(/"experience_date":"[^"]*"/g, '"experience_date":"2099-09-29"');
+
+} else if (url.indexOf(grzx) != -1) {
+    body = body.replace(/"title":"[^"]*"/g, '"title":"免费脚本请勿购买"');
+
+}
+$done({ body });
+
+//by ios151
