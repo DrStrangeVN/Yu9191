7cc4c860a690caa2bb5576ac00993b7a536f6d62
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-01-04 00:54:25 +0800

    Create Xcz.js

---
+++ Xcz.js
@@ -0,0 +1,20 @@
+/*
+
+西窗烛,诗词之美
+
+汉字的故事URL
+https://avoscloud.com/1.1/users/65958f9d4f6620374815b995
+[rewrite_local]
+#西窗烛,诗词之美
+^https?:\/\/lchttpapi\.xczim\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+#汉字的故事
+^https?:\/\/avoscloud\.com\/1.1\/users url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xcz.js
+[MITM]
+hostname = lchttpapi.xczim.com, avoscloud.com
+
+
+*/
+var Q = JSON.parse($response.body);
+Q.membership = true;
+Q.lifetimeMembership = true;
+$done({ body: JSON.stringify(Q) });
