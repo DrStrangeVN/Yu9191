178c99c4ef4dac91d068e4f4c05a6913fa2c6570
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-01-09 22:02:51 +0800

    Update Xiaoyejiazigu.js

--- Xiaoyejiazigu.js
+++ Xiaoyejiazigu.js
@@ -1,11 +1,9 @@
 /*
 架子鼓 3.2.0
-time：2023-12-25
+time：2024-1-9
 
 [rewrite_local]
-#正则合并
-^https:\/\/oneplay-api\.xiaoyezi\.com\/drum\/(score|account) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xiaoyejiazigu.js
-
+^https:\/\/oneplay-api\.xiaoyezi\.com\/drum\/(score|account|course\/user_behaviour) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Xiaoyejiazigu.js
 [mitm]
 hostname = oneplay-api.xiaoyezi.com
 */
@@ -17,8 +15,10 @@ if (url.indexOf("/drum/score/") != -1){
     body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
     body = body.replace(/"account_status":\s*0,/g, "\"account_status\": 1,");//是否拥有
     body = body.replace(/"product_type":\s*"1"/g, "\"product_type\": \"0\"");
-
-
+}
+if (url.indexOf("/drum/course/user_behaviour") != -1){
+    body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
+    body = body.replace(/"process_rate":0/g, "\"process_rate\":100");//进度
 } else if (url.indexOf("/drum/account") != -1){
     let obj = JSON.parse(body);
     obj.data.subscribe = {
