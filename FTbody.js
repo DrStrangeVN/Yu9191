1bf3e42a57075b930d9277357468ae5c4ec106d9
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-01-27 06:22:09 +0800

    更新 FTbody.js

--- FTbody.js
+++ FTbody.js
@@ -7,4 +7,4 @@ body = body.replace(/"paywall":\d+/g, '"paywall":1');
 // 替换 "accessright":"\d+ 为 "accessright":"1
 body = body.replace(/"accessright":"\d+/g, '"accessright":"1');
 
-$done(body);
+$done({ body });
