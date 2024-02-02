1f4f00a9f32f50228024623874c8f818403121b1
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-02-02 20:09:38 +0800

    更新 memrise.js

--- memrise.js
+++ memrise.js
@@ -17,6 +17,6 @@ body = body.replace(/"is_pro":\s*false/g, '"is_pro":true');
 
 body = body.replace(/"is_premium":\s*false/g, '"is_premium":true');
 
-body = body.replace(/"is_locked":\s*false/g, '"is_locked":true');
+//body = body.replace(/"is_locked":\s*false/g, '"is_locked":true');
 
 $done({ body });
