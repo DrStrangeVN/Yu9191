303b1366661765f8d148afc9fe6be4a57218645e
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-02-15 22:43:28 +0800

    更新 Xmbsbk.js

--- Xmbsbk.js
+++ Xmbsbk.js
@@ -3,7 +3,7 @@
 [rewrite_local]
 
 #解锁会员开关
-^https://lp-api.xiongmaoboshi.com/lp-api/ns url script-response-body https://https://raw.githubusercontent.com/Yu9191/Rewrite/main/xiongmaoboshibaike.js
+^https://lp-api.xiongmaoboshi.com/lp-api/ns url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/xiongmaoboshibaike.js
 #获取完整视频
 ^https:\/\/lp-vid\.xiongmaoboshi\.com\/.*\.m3u8$ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/xiongmaoboshibaike.js
 
