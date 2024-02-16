9e5f441f5b37cf37425d1f3cdfb49404e6868958
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-02-16 18:24:31 +0800

    更新 hejihuaxl.js

--- hejihuaxl.js
+++ hejihuaxl.js
@@ -1,7 +1,7 @@
 /**
 
 脚本名称: 和计划心理
-
+价格四位数的课程无法解锁
 [rewrite_local]
 
 https://cgzd.hejihua.com/api/teaching/project/catalog/v1 url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/hejihuaxl.js
