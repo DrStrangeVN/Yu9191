d0f88a77ff059ce91c4b310fb3d976fda7efb59b
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-02-22 14:45:49 +0800

    更新 xiaobaixuexi.js

--- xiaobaixuexi.js
+++ xiaobaixuexi.js
@@ -24,15 +24,23 @@ hostname = api.xbxxhz.com, epbox.gongfudou.com
 */
 
 var body = $response.body;
+
+// 替换 "free":\w+
 body = body.replace(/"free":\w+/g, '"free":true');
+
+// 替换 "isSchoolAgeMember":\w+
 body = body.replace(/"isSchoolAgeMember":\w+/g, '"isSchoolAgeMember":true');
+
+// 替换 "isNormalMember":\w+
 body = body.replace(/"isNormalMember":\w+/g, '"isNormalMember":true');
+
+// 替换 "baseMemberAvailable":\w+
 body = body.replace(/"baseMemberAvailable":\w+/g, '"baseMemberAvailable":false');
 
-if (body.includes('"schoolAgeMember"')) {
-    body = body.replace(/"expiresAt": "\d{4}年\d{1,2}月\d{1,2}日"/g, '"expiresAt": "2099年9月9日"');
-} else {
-    body = body.replace(/"selectedKid" : {/g, '"selectedKid" : { "schoolAgeMember" : { "expiresAt" : "2099年9月9日", "__typename" : "Member" },');
-}
+// 替换 "expiresAt": "\d{4}年\d{1,2}月\d{1,2}日"
+body = body.replace(/"expiresAt": "\d{4}年\d{1,2}月\d{1,2}日"/g, '"expiresAt": "2099年9月9日"');
+
+// 替换 "selectedKid" : {
+body = body.replace(/"selectedKid" : {/g, '"selectedKid" : { "schoolAgeMember" : { "expiresAt" : "2099年9月9日", "__typename" : "Member" },');
 
 $done({ body });
