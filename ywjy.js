e0c4552d1fabb480d244cc157f7f207631903a39
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-02-26 13:17:55 +0800

    更新 ywjy.js

--- ywjy.js
+++ ywjy.js
@@ -2,7 +2,7 @@
 羿文教育
 课程无法解决
 题库 下载资源都没问题
-
+tip：如果您搬运借鉴请备注来源 虽然简单 毕竟花费时间啦
 [rewrite_local]
 #课程题库
 https://proxyweb\.yiwenjy\.com/yiwen_mobile/(queryApp|query_classDetail_info) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/ywjy.js
