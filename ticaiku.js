59a027e3718cbff5052071f3ec8a83aeb56988e9
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-03-14 08:36:05 +0800

    更新 ticaiku.js

--- ticaiku.js
+++ ticaiku.js
@@ -18,16 +18,10 @@ hostname = miniapp.guniuniu.com
 
 
 */
-
-
-
-
-    
-
-    let obj = JSON.parse($response.body);    
-    obj.data.nickName = "Baby";  
-    obj.data.vipTime = "2099-09-09 02:20:13";             
-    obj.data.svipTime = "2099-09-09 02:20:13";
-    $done({
-        body: JSON.stringify(obj)
-    });
+let obj = JSON.parse($response.body);
+obj.data.nickName = "Baby";
+obj.data.vipTime = "2099-09-09 02:20:13";
+//obj.data.svipTime = "2099-09-09 02:20:13";
+$done({
+    body: JSON.stringify(obj)
+});
