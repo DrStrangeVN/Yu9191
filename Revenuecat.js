e74d8ae50db27571dfa8b2da6cbf1a1f8b83ab2a
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-03-21 22:13:39 +0800

    更新 Revenuecat.js

--- Revenuecat.js
+++ Revenuecat.js
@@ -63,6 +63,7 @@ if (typeof $response == "undefined") {
   var UA = $request.headers['user-agent'];
   const app = '1';
   const UAMappings = {
+    'PDF%20Viewer':{ name: 'sub.pro', id: 'com.pspdfkit.viewer.sub.pro.yearly'},//2024.3.21
     'Text%20Workflow':{ name: 'pro', id: 'tw_99_1m'},//2024.3.2
     'FaceMa':{ name: 'Pro access', id: 'Pro_Lifetime'},//Facemo
     'MadeYu':{ name: 'pro_plus', id: 'my_549_1m_400'},//
