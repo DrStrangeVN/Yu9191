d24f71e0ac7c7ef0b34acd4786a3b22535fd97b2
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-03-22 22:13:15 +0800

    更新 Laiyin.js

--- Laiyin.js
+++ Laiyin.js
@@ -8,28 +8,23 @@
 [mitm]
 hostname = partner-ai-api.quthing.com
 */
-var body = $response.body;
 var url = $request.url;
-//会员
-if (url.indexOf('vip/state') != -1) {
+var body = $response.body;
+
+if (url.indexOf('student/info/h5') !== -1) {
     var obj = JSON.parse(body);
-    obj.data.seniorVip = true;
-    obj.data.showText = "2099.09.09到期";
-    obj.data.validVip = true;
-    obj.data.expireTime = 4092595200000;
-    obj.data.vipCount = 205889;
-    obj.data.lastBuySkuName = "partnertrainingai_yaer_vip";
-    body = JSON.stringify(obj);
-} else if (url.indexOf('student/info/h5') != -1) {
-    var obj = JSON.parse(body);
+
+    
+
     obj.data.vipInfo.seniorVip = true;
     obj.data.vipInfo.showText = "2099.09.09到期";
     obj.data.vipInfo.validVip = true;
     obj.data.vipInfo.expireTime = 4092595200000;
     obj.data.vipInfo.vipCount = 205889;
     obj.data.vipInfo.lastBuySkuName = "partnertrainingai_yaer_vip";
+    
+
     body = JSON.stringify(obj);
-
 }
-$done({ body });
 
+$done({ body });
