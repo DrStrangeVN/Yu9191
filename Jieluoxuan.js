ba0d01bb492ac8ed93585b62284abb04323e5fb5
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-03-28 15:51:09 +0800

    Update Jieluoxuan.js

--- Jieluoxuan.js
+++ Jieluoxuan.js
@@ -5,7 +5,7 @@
 [rewrite_local]
 
 ^https:\/\/app\.helixlife\.cn\/api\/v1\/(user\/overviews|edu\/(trainings|courses)) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Jieluoxuan.js
-https://app.helixlife.cn/api/v1/user/users/profile url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Jieluoxuanmy.js
+https://app.helixlife.cn/api/v1/user/users/profile url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Jieluoxianmy.js
 [mitm]
 hostname = app.helixlife.cn
 */
