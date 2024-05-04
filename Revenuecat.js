83e5379bf523631ee9a91d95ccbbd6072f044ca6
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-05-05 06:55:14 +0800

    更新 Revenuecat.js

--- Revenuecat.js
+++ Revenuecat.js
@@ -63,6 +63,7 @@ if (typeof $response == "undefined") {
   var UA = $request.headers['user-agent'];
   const app = '1';
   const UAMappings = {
+    'No%20Fusion':{ name: 'LivePhoto', id: 'com.grey.livephoto.reference.price'},//2024.5.5
     'mark_cup':{ name: 'premiun', id: '202403180021'},//20.24.5.4
     'VOX':{ name: 'VOX Premium', id: 'com.coppertino.VoxMobile.AU.Loop1_v8'},//20.24.4.22
     'PDF%20Viewer':{ name: 'sub.pro', id: 'com.pspdfkit.viewer.sub.pro.yearly'},//2024.3.21
