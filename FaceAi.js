55d6151970a428131ee7b809485fde1be561e45e
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-05-05 21:13:03 +0800

    更新 FaceAi.js

--- FaceAi.js
+++ FaceAi.js
@@ -2,7 +2,7 @@
 
 faceai 
 ‎Watch Faces GallerY 4.93
-
+无人机助手
 [rewrite_local]
 
 https://api.adapty.io/api/v1/sdk/in-apps/apple/receipt/validate/ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/FaceAi.js
